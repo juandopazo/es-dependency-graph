c087875d2a820d09f311ce49911ce0a66b9c9362
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2014-05-09 16:25:34 -0300

    Move CLI code to lib/ for testing

--- bin/module-graph.js
+++ bin/module-graph.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
+var cli = require('../lib/cli');
 
-var graph  = require('../lib/index'),
-    extend = require('extend'),
-    path   = require('path'),
-    fs     = require('fs');
-
-var options = require('nomnom')
+cli(require('nomnom')
     .options({
         path: {
             position: 0,
@@ -24,31 +20,4 @@ var options = require('nomnom')
         }
     })
     .nocolors()
-    .parse();
-
-var dependencies = {};
-
-options.path
-    .forEach(function parse(filepath) {
-        var stat = fs.statSync(filepath),
-            sources = {};
-
-        if (stat.isFile() && path.extname(filepath) === '.js') {
-            sources[path.basename(filepath, '.js')] =
-                fs.readFileSync(filepath, 'utf8');
-            extend(dependencies, graph(sources));
-        } else if (stat.isDirectory()) {
-            fs.readdirSync(filepath).forEach(function (filename) {
-                var subpath = path.join(filepath, filename),
-                    substat = fs.statSync(subpath);
-
-                if (substat.isFile() || (substat.isDirectory() && options.walk)) {
-                    parse(subpath);
-                }
-            });
-        }
-    });
-
-dependencies = JSON.stringify(dependencies);
-
-fs.writeFileSync(options.output, dependencies, 'utf8');
+    .parse());
