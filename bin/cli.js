4d3bf0ed604fcd7a4d1f24b45c8629d488438287
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2014-05-09 16:06:15 -0300

    Add command line interface

---
+++ bin/cli.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+var graph  = require('../lib/index'),
+    extend = require('extend'),
+    path   = require('path'),
+    fs     = require('fs');
+
+var options = require('nomnom')
+    .options({
+        path: {
+            position: 0,
+            help: 'Files or folders to read',
+            list: true
+        },
+        walk: {
+            abbr: 'w',
+            help: 'Walk the folder structure looking for more files',
+            default: false
+        },
+        output: {
+            abbr: 'o',
+            metavar: 'FILE',
+            help: 'File to write the dependency information to'
+        }
+    })
+    .nocolors()
+    .parse();
+
+var dependencies = {};
+
+options.path
+    .map(function (relPath) {
+        return path.resolve(__dirname, relPath);
+    })
+    .forEach(function parse(filepath) {
+        var stat = fs.statSync(filepath),
+            sources = {};
+
+        if (stat.isFile() && path.extname(filepath) === '.js') {
+            sources[path.basename(filepath, '.js')] =
+                fs.readFileSync(filepath, 'utf8');
+            extend(dependencies, graph(sources));
+        } else if (stat.isDirectory()) {
+            fs.readdirSync(filepath).forEach(function (filename) {
+                var subpath = path.join(filepath, filename),
+                    substat = fs.statSync(subpath);
+
+                if (substat.isFile() || (substat.isDirectory() && options.walk)) {
+                    parse(subpath);
+                }
+            });
+        }
+    });
+
+dependencies = JSON.stringify(dependencies);
+
+fs.writeFileSync(path.resolve(__dirname, options.output), dependencies, 'utf8');
